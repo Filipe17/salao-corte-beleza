d649b418a70f7408099e373107a70e180a655748
Filipe17 <62030711+Filipe17@users.noreply.github.com>
2026-09-10 16:23:22 -0300

    Add files via upload

--- static/js/pages.js
+++ static/js/pages.js
@@ -6159,7 +6159,7 @@ function estRenderPainel(p) {
         Editar
       </button>
       <button class="btn btn-primary" onclick="entradaEstoque(${p.id})">
-        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" width="13" height="13"><path d="M21 15v4a2 2 0 01-2 2H5a2 2 0 01-2-2v-4"/><polyline points="17 8 12 3 7 8"/><line x1="12" y1="3" x2="12" y2="15"/></svg>
+        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" width="12" height="12"><path d="M21 15v4a2 2 0 01-2 2H5a2 2 0 01-2-2v-4"/><polyline points="17 8 12 3 7 8"/><line x1="12" y1="3" x2="12" y2="15"/></svg>
         Movimentação
       </button>
       <button class="btn est-btn-excluir" onclick="excluirProduto(${p.id})">
